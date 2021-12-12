788d098486daa114ff81742c798832478cb403ba
Jinyu Wang <ellie5201314@gmail.com>
2021-12-12 16:45:35 -0500

    firebase

---
+++ FirebaseLogin/Controller/LoginViewController.swift
@@ -0,0 +1,47 @@
+//
+//  LoginViewController.swift
+//  FirebaseLogin
+//
+//  Created by Yu Jin on 12/12/21.
+//
+
+import UIKit
+import Firebase
+class LoginViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        //Optional Binding
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                }
+                else{
+                    self.performSegue(withIdentifier: "loginToChat", sender: self)
+                }
+                // ...
+            }
+        }
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FirebaseLogin/Controller/RegisterViewController.swift
@@ -0,0 +1,63 @@
+//RegisterViewController
+//  RegisterViewController.swift
+//  FirebaseLogin
+//
+//  Created by Yu Jin on 12/12/21.
+//
+
+import UIKit
+import Firebase
+class RegisterViewController: UIViewController {
+
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        //Optional Binding -> If Let
+        //Guard Statement -> Guard Let
+        
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                
+                if let e = error {
+                    print(e.localizedDescription)
+                }
+                else {
+                    self.performSegue(withIdentifier: "registerToChat", sender: self)
+                    
+                }
+                // ...
+                
+            }
+        
+        
+        
+        }
+        
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
